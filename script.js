788b402d6b42fef7f542194b815859b04fa78d2c
Arturda9 <24963573-Arturda9@users.noreply.replit.com>
2023-08-31 10:30:56 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+//notas = Array(6);
+//soma = 0
+//for(i= 0;i<6;i++)
+//  {
+//notas[i]=prompt("Informe a nota do aluno" + i)
+//   soma=soma+parseInt(notas[i]);
+// }
+//alert("Notas da turma: " + notas)
+//alert("Nota máxima:" + Math.max.apply(Math,notas))
+//alert("Nota mínima:" + Math.min.apply(Math,notas))
+//alert("Nota média:" + soma/notas.length)
+
+
+//x = parseFloat(prompt("informe um número maior que 0 :"));
+//alert("Math.log" + Math.log(x))
+//alert("Math.log2" + Math.log2(x))
+//alert("Math.log10" + Math.log10(x))
+
+//let a = parseInt(prompt("insira o valor de a"))
+//let b = parseInt(prompt("insira o vlaor de b"))
+//let c = parseInt(prompt("insira o valor de c"))
+//delta = Math.pow(b,2) - 4*a*c
+
+//if (delta<0){
+//  alert("não possui raizes reais")
+//}
+//else {
+ // x1=(-b+Math.sqrt(delta))/ 2*a
+ // x2=(-b-Math.sqrt(delta))/ 2*a
+ // alert("x1 = " + x1)
+  //alert("x2 = " + x2)
+//}
+
+alert("bem-vindo ao Github")
+
+
+
